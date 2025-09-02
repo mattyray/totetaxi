0c2c8587a22246f96d418e346feb34197d5f2bb0
mattyray <mnraynor90@gmail.com>
2025-09-02 19:15:49 -0400

    urls.py in config

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/customer/', include('apps.customers.urls')),
+    path('api/public/', include('apps.bookings.urls')),  # Add this line
 ]
