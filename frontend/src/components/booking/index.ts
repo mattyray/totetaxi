6f8629765d14a4e45ac91f1ccf94cc0212b9da19
mattyray <mnraynor90@gmail.com>
2025-09-09 17:15:47 -0400

    WIZARD BUILDOUT - index exportas

---
+++ frontend/src/components/booking/index.ts
@@ -0,0 +1,6 @@
+export { BookingWizard } from './booking-wizard';
+export { ServiceSelectionStep } from './service-selection-step';
+export { DateTimeStep } from './date-time-step';
+export { AddressStep } from './address-step';
+export { CustomerInfoStep } from './customer-info-step';
+export { ReviewPaymentStep } from './review-payment-step';
