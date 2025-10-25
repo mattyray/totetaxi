b2622ee9117d0e922e5eac95bcd10c320b3f3dd8
mattyray <mnraynor90@gmail.com>
2025-10-25 10:31:25 -0400

    sentry integration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,10 @@
-// frontend/next.config.ts
+// frontend/next.config.ts - UPDATE YOUR FILE TO LOOK LIKE THIS:
+
+import { withSentryConfig } from '@sentry/nextjs';
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Existing config
+  // Your existing config
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -79,18 +81,48 @@ const nextConfig: NextConfig = {
   experimental: {
     // Enable modern bundling
     esmExternals: true
-  },
-
-  // OVERRIDE: Force disable linting in all contexts
-  webpack: (config, { dev }) => {
-    // Disable ESLint plugin entirely in production builds
-    if (!dev) {
-      config.plugins = config.plugins.filter(
-        (plugin: any) => plugin.constructor.name !== 'ESLintWebpackPlugin'
-      );
-    }
-    return config;
   }
 };
 
-export default nextConfig;
+const sentryWebpackPluginOptions = {
+  // Only upload source maps in production
+  silent: true,
+  widenClientFileUpload: true,
+  hideSourceMaps: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+};
+
+// Export with Sentry configuration
+export default withSentryConfig(nextConfig, {
+  // For all available options, see:
+  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+
+  org: "matthew-raynor",
+
+  project: "totetaxi-next",
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  // tunnelRoute: "/monitoring",
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true
+});
---
+++ frontend/sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://2d9a44516c49300f04125ed8775c7450@o4509753799409664.ingest.us.sentry.io/4510246779420672",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
